174f630ef11fc8654153d7cedcdb26da3536a608
Ian Mooney <ian@yoomee.com>
2012-09-03 16:27:11 +0100

    Run ym gem installers

--- template.rb
+++ template.rb
@@ -121,3 +121,12 @@ inside('config') do
   end
   
 end
+
+run('bundle')
+run('bundle exec rake db:create')
+run('bundle exec rails g ym_core:install')
+run('bundle exec rails g ym_cms:install')
+run('bundle exec rails g ym_users:install')
+run('bundle exec rails g ym_permalinks:install')
+run('bundle exec rake db:migrate')
+run('bundle exec rake db:seed')
