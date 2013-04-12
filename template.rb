660ed4cffb8b50e987026748e8a31f8ebd806863
Matt Atkins <matt@yoomee.com>
2013-04-12 12:30:21 +0100

    Start upgrade to rails 3.2

--- template.rb
+++ template.rb
@@ -3,62 +3,62 @@ require 'yaml'
 route "root :to => 'home#index'"
 add_source 'https://yoomee:wLjuGMTu30AvxVyIrq3datc73LVUkvo@gems.yoomee.com'
 
-gem "rake", "0.8.7"
 gem 'exception_notification'
-gem 'country-select'
 gem 'formtastic-bootstrap', :git => "git://github.com/cgunther/formtastic-bootstrap.git", :branch => "bootstrap-2"
 gem 'whenever', :require => false
-gem 'ym_core'
-gem 'ym_cms'
-gem 'ym_users'
-gem 'ym_permalinks'
+gem "ym_core", :git => 'git@gitlab.yoomee.com:ym_core.git', :branch => 'rails-3-2'
+gem "ym_videos", :git => 'git@gitlab.yoomee.com:ym_core.git', :branch => 'rails-3-2'
+gem "ym_cms", :git => 'git@gitlab.yoomee.com:ym_core.git', :branch => 'rails-3-2'
+gem "ym_users", :git => 'git@gitlab.yoomee.com:ym_core.git', :branch => 'rails-3-2'
+gem "ym_permalinks", :git => 'git@gitlab.yoomee.com:ym_core.git', :branch => 'rails-3-2'
 
-inside(".") do
-  open("Gemfile", 'a') do |file|
-    file << "\ngroup :development do\n"
-    file << "  gem 'growl'\n"
-    file << "  gem 'mailcatcher'\n"
-    file << "  gem 'ruby-debug19'\n"
-    file << "  gem 'ym_tools'\n"
-    file << "  gem 'passenger'\n"
-    file << "end\n"
-    file << "group :development, :test do\n"
-    file << "  gem 'rspec-rails'\n"
-    file << "end\n"
-    file << "group :test do\n"
-    file << "  gem 'factory_girl_rails'\n"
-    file << "  gem 'shoulda-matchers'\n"
-    file << "  gem 'capybara'\n"
-    file << "  gem 'guard-rspec'\n"
-    file << "  gem 'sqlite3'\n"
-    file << "end\n"
-  end
+# inside(".") do
+#   open("Gemfile", 'a') do |file|
+#     file << "\ngroup :development do\n"
+#     file << "  gem 'growl'\n"
+#     file << "  gem 'mailcatcher'\n"
+#     file << "  gem 'ruby-debug19'\n"
+#     file << "  gem 'ym_tools'\n"
+#     file << "  gem 'passenger'\n"
+#     file << "end\n"
+#     file << "group :development, :test do\n"
+#     file << "  gem 'rspec-rails'\n"
+#     file << "end\n"
+#     file << "group :test do\n"
+#     file << "  gem 'factory_girl_rails'\n"
+#     file << "  gem 'shoulda-matchers'\n"
+#     file << "  gem 'capybara'\n"
+#     file << "  gem 'guard-rspec'\n"
+#     file << "  gem 'sqlite3'\n"
+#     file << "end\n"
+#   end
+# end
+
+#TODO: with rails 3.2 this will work instead of the above
+gem_group :development do
+ gem 'growl'
+ gem 'letter_opener'
+ gem 'ruby-debug19'
+ gem 'ym_tools'
+ gem 'passenger'
 end
-
-# TODO: with rails 3.2 this will work instead of the above
-# gem_group :development do
-#  gem 'growl'
-#  gem 'mailcatcher'
-#  gem 'ruby-debug19'
-#  gem 'ym_tools'
-#  gem 'passenger'
-# end
-# gem_group :development, :test do
-#   gem "rspec-rails"
-# end
-# gem_group :test do
-#  gem "factory_girl_rails"
-#  gem 'shoulda-matchers'
-#  gem "capybara"
-#  gem "guard-rspec"
-#  gem "sqlite3"
-# end
+gem_group :development, :test do
+  gem "rspec-rails"
+end
+gem_group :test do
+ gem "factory_girl_rails"
+ gem 'shoulda-matchers'
+ gem "capybara", "~> 1.1.4"
+ gem "guard-rspec"
+ gem "sqlite3"
+end
 
 run("rm public/index.html")
 run("rm app/assets/images/rails.png")
 run("rm app/assets/javascripts/application.js")
 run("rm app/views/layouts/application.html.erb")
 run("cp #{ENV['HOME']}/Rails/conan/gitignore .gitignore")
+run("cp #{ENV['HOME']}/Rails/conan/rvmrc .rvmrc")
 
 Dir["#{ENV['HOME']}/Rails/conan/*/"].each do |directory_path|
   directory = directory_path.split("/").last
@@ -91,17 +91,13 @@ inside('config') do
   file_text = File.read('environments/development.rb')
   open('environments/development.rb', 'w') do |file|
     file << file_text.gsub(/end$/, "  config.action_mailer.default_url_options = {:host => 'localhost:3000'}\n")
-    # file << "\nconfig.action_mailer.default_url_options = {:host => 'localhost:3000'}\n"
-    file << "  # Send email to mailcatcher\n"
-    file << "  config.action_mailer.delivery_method = :smtp\n"
-    file << "  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }\n"
+    file << "  config.action_mailer.delivery_method = :letter_opener\n"
     file << "end"
   end
 
   file_text = File.read('environments/production.rb')
   open('environments/production.rb', 'w') do |file|
     file << file_text.gsub(/end$/, "  config.action_mailer.smtp_settings = {\n")
-    # file << "\nconfig.action_mailer.smtp_settings = {\n"
     file << "    :address        => 'mail.studentbabble.com',\n"
     file << "    :domain         => 'mail.studentbabble.com',\n"
     file << "    :authentication => :login,\n"
@@ -132,15 +128,15 @@ inside('config') do
 end
 
 run('bundle')
-run('bundle exec rake db:create')
-run('bundle exec rails g ym_core:install')
-run('bundle exec rails g ym_cms:install')
-run('bundle exec rails g ym_users:install')
-run('bundle exec rails g ym_permalinks:install')
+rake('db:create')
+generate('ym_core:install')
+generate('ym_cms:install')
+generate('ym_users:install')
+generate('ym_permalinks:install')
 
 if yes?("Run migrations?")
-  run('bundle exec rake db:migrate')
+  rake('db:migrate')
   if yes?("Run seeds?")
-    run('bundle exec rake db:seed')
+    rake('db:seed')
   end
 end
