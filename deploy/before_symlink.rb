8c38fc707d677307d3a9908928810de116b08787
Edward Andrews-Hodgson <edward@yoomee.com>
2013-12-20 15:40:21 +0000

    Add dragonfly and thinking sphinx symlinking deploy hooks

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,16 @@
 FileUtils::mkdir_p "#{shared_path}/dragonfly"
 FileUtils::mkdir_p "#{release_path}/data"
 run "ln -s #{shared_path}/dragonfly #{release_path}/data/dragonfly"
+
+# symlink cache
+FileUtils::mkdir_p "#{shared_path}/cache"
+FileUtils::rmdir "#{shared_path}/cache/assets"
+FileUtils::rmdir "#{release_path}/tmp/cache"
+FileUtils::mkdir_p "#{release_path}/tmp"
+run "ln -s #{shared_path}/cache #{release_path}/tmp/cache"
+
+# Setup sphinx
+run "rm -rf #{release_path}/db/sphinx"
+run "mkdir -p #{shared_path}/sphinx"
+run "ln -nfs #{shared_path}/sphinx #{release_path}/db/sphinx"
+run "ln -nfs #{shared_path}/production.sphinx.conf #{release_path}/config/production.sphinx.conf"
